efb19fd8194960b18cd5918c1c91cf8f72e320e4
Observed Observer <MasterHaoChen@icloud.com>
2018-07-20 23:19:04 +0800

    first demo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {spliter} from './utils/translator.js'
+import {dataSource, Dimensions, Measures} from './data/test.js'
 import './App.css';
+import G2 from '@antv/g2'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      mdxString: ''
+    }
+  }
+  componentDidMount () {
+    this.chart = new G2.Chart({
+      container: 'g2-chart',
+      width: 600,
+      height: 400,
+      padding: [40, 80, 80, 80]
+    });
+  }
+  getMDX = (ev) => {
+    console.log(ev.target.value)
+    let mdx = ev.target.value
+    let words = spliter(mdx)
+    this.setState({
+      mdxString: (<p>SELECT [<span style={{color: 'red'}}>{words.row}</span>] ON ROW,<br/>
+       [<span style={{color: 'green'}}>{words.column}</span>] ON COLUMN <br/>
+       FROM [<span style={{color: 'orange'}}>{words.cube}</span>] <br/>
+       WHERE [<span style={{color: 'blue'}}>{words.measure}</span>]</p> ),
+       g2String: (<p>
+        chart.source({words.cube})
+        chart.interval().position('{words.row}*{words.measure}').color('{words.column}')
+        </p>)
+    })
+    console.log(spliter(mdx))
+    const {row, column, measure, cube} = words
+    if (row && column && measure && cube) {
+      console.log('render chart')
+      
+      this.chart.source(dataSource())
+      this.chart.facet('rect', {
+        fields: [column],
+        eachView: function eachView(view) {
+          view.interval().position(row + '*' + measure).color(row)
+        }
+      });
+      this.chart.render()
+    } else {
+      this.chart.clear()
+    }
+  }
   render() {
+    let dims = Object.keys(Dimensions).join(' ; ')
+    let meas = Object.keys(Measures).join(' ; ')
+    // this.chart.render()
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">MDX to G2</h1>
         </header>
+        <p>Cube: dataSource</p>
+        <p>维度: {dims}</p>
+        <p>度量: {meas}</p>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        : )
         </p>
+        <h3>Your Mdx:</h3>
+        {this.state.mdxString}
+        <textarea onChange={this.getMDX} className="App-Input"></textarea>
+        <h3>Your Chart:</h3>
+        <div id="g2-chart"></div>
       </div>
     );
   }
---
+++ src/data/test.js
@@ -0,0 +1,28 @@
+const Len = 20;
+const Dimensions = {
+    'department': ['优酷', '爱奇艺', 'B站'], 
+    'month': ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], 
+    'video': ['美剧', '动漫', '抗日神剧']
+}
+const Measures = {
+    'profit': [0, 10000], 
+    'count': [0, 200]
+}
+function dataSource () {
+    let i, j, data = [], item;
+    let dims = Object.keys(Dimensions)
+    let meas = Object.keys(Measures)
+    for (i = 0; i < Len; i++) {
+        item = {}
+        dims.forEach((dim) => {
+            item[dim] = Dimensions[dim][parseInt(Math.random() * Dimensions[dim].length)]
+        })
+        meas.forEach((mea) => {
+            item[mea] = Measures[mea][0] + parseInt(Math.random() * (Measures[mea][1] - Measures[mea][0]))
+        })
+        data.push(item)
+    }
+    console.log(data)
+    return data
+}
+export {dataSource, Dimensions, Measures}
